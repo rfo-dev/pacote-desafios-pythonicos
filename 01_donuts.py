0d6e5731709f541de2dde30c67ed289bea8ed5ef
rafael.oliveira <rafael.foliveira@al.infnet.edu.br>
2021-11-21 22:55:42 -0300

    "Realizado"

--- 01_donuts.py
+++ 01_donuts.py
@@ -10,8 +10,11 @@ e donuts(23) retorna 'Number of donuts: many'
 """
 
 def donuts(count):
-    # +++ SUA SOLUÇÃO +++
-    return
+
+    if count < 10:
+        return (f'Number of donuts: {count}')
+    return "Number of donuts: many"
+
 
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
